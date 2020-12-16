6ace03377573ebcddc4ea2da5375d66c65f0083c
TeresaChou <chowduen@gmail.com>
2020-12-16 14:28:55 +0800

    testing finite state machine

--- BasicInfoSetting.py
+++ BasicInfoSetting.py
@@ -1,8 +1,9 @@
 from linebot import (LineBotApi, WebhookHandler)
 from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import *
+from transitions import Machine
 
-def BasicInfoSetting():
+def BasicInfoSettingEntrance():
     message = TemplateSendMessage(
         alt_text='基本資料設定',
         template=CarouselTemplate(
@@ -11,8 +12,8 @@ def BasicInfoSetting():
                     text='全部重新設定',
                     actions=[
                         MessageTemplateAction(
-                            label='全部重新設定',
-                            text='點我重新設定'
+                            label='點我重新設定',
+                            text='全部重新設定'
                         )
                     ]
                 ),
@@ -20,8 +21,8 @@ def BasicInfoSetting():
                     text='查看目前設定',
                     actions=[
                         MessageTemplateAction(
-                            label='查看目前設定',
-                            text='點我查看'
+                            label='點我查看',
+                            text='查看目前設定'
                         )
                     ]
                 ),
@@ -35,8 +36,8 @@ def BasicInfoSetting():
                         #     data='將這個訊息偷偷回傳給機器人'
                         # ),
                         MessageTemplateAction(
-                            label='設定住家地址',
-                            text='點我設定'
+                            label='點我設定',
+                            text='設定住家地址'
                         )
                         # URITemplateAction(
                         #     label='進入1的網頁',
@@ -48,8 +49,8 @@ def BasicInfoSetting():
                     text='設定常用地點',
                     actions=[
                         MessageTemplateAction(
-                            label='設定常用地點',
-                            text='點我設定'
+                            label='點我設定',
+                            text='設定常用地點'
                         )
                     ]
                 ),
@@ -57,8 +58,8 @@ def BasicInfoSetting():
                     text='設定緊急聯絡人',
                     actions=[
                         MessageTemplateAction(
-                            label='設定緊急聯絡人',
-                            text='點我設定'
+                            label='點我設定',
+                            text='設定緊急聯絡人'
                         )
                     ]
                 )
@@ -66,3 +67,23 @@ def BasicInfoSetting():
         )
     )
     return message
+
+def BasicInfoSetting(event, BISM):
+    BISM.sleep()
+    return "設定完成"
+
+class BasicInfoStateMachine(object):
+
+    states = ['default', 'waiting', 'home', 'often', 'contact', 'all_home', 'all_often']
+
+    def __init__(self):
+        self.machine = Machine(model=self, states=BasicInfoStateMachine.states, initial='default')
+
+        # add_transition(trigger, source, dest)
+        self.add_transition('sleep', '*', 'default')
+        self.add_transition('start', '*', 'waiting')
+        self.add_transition('setting_home', 'waiting', 'home')
+        self.add_transition('setting_often', 'waiting', 'often')
+        self.add_transition('setting_contact', '*', 'contact')
+        self.add_transition('all_setting_home', '*', 'all_home')
+        self.add_transition('all_setting_often', 'all_home', 'all_often')
--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import requests
 # import pandas as pd
 # import pyimgur
 # from selenium import webdriver
-# import mysql.connector
-# from mysql.connector import Error
+import mysql.connector
+from mysql.connector import Error
 
 
 from flask import Flask, request, abort
@@ -55,72 +55,92 @@ def callback():
         abort(400)
     return 'OK'
 
+
+BISMList = {}   # the state machine of BasicInfoSetting
+
+def resetAllMachine(user_id):
+    BISMList[user_id].sleep()
     
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    message = ""
     msg = event.message.text
     user_id = event.source.user_id
-    if 'newswebsite' in msg:
-        message = imagemap_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '基本資料設定' in msg:
-        message = BasicInfoSetting()
-        line_bot_api.reply_message(event.reply_token, message)
+    if user_id not in BISMList:
+        BISMList[user_id] = BasicInfoStateMachine()
+
+    if '基本資料設定' in msg:
+        resetAllMachine(user_id)
+        BISMList[user_id].start()
+        message = BasicInfoSettingEntrance()
     elif '查詢警示地點' in msg:
+        resetAllMachine(user_id)
         note = '請按下方的+號按鈕，然後傳送要查詢的位置'
         message = TextSendMessage(text=note)
-        line_bot_api.reply_message(event.reply_token, message)
     elif '開始回家' in msg:
+        resetAllMachine(user_id)
         message = TextSendMessage(text=msg)
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '旋轉木馬' in msg:
-        message = Carousel_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '圖片畫廊' in msg:
-        message = test()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '功能' in msg:
-        a='1:輸入 『rate』 得知美國公債報價、銀行拆借利率、FED利率、Tips\n2:輸入 『worldequity』\n得知全球股票市場和指數期貨市場報價\n3:輸入 『twstock+股票代碼』\n得知該台股2019年走勢\n4:輸入 『news』\n得知台股與國際股市新聞\n5:輸入 『sectors』\n獲取美股各產業漲跌幅\n6:輸入 『commodity』 取得原物料最新報價\n7:輸入 『BIresearch』 獲取Bloomberg Intelligence研究報告\n8:輸入 『equityprimer』   獲取Bloomberg Intelligence個股研究報告\n9:輸入 『newswebsite』\n進入財經新聞網站\n10:輸入 『tvshows』\n觀看彭博社精選節目'
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=a))
+    elif BISMList[user_id].state != 'default':
+        message = BasicInfoSetting(event, BISMList[user_id])
+    # elif 'newswebsite' in msg:
+    #     message = imagemap_message()
+    #     line_bot_api.reply_message(event.reply_token, message)
+    # elif '旋轉木馬' in msg:
+    #     message = Carousel_Template()
+    #     line_bot_api.reply_message(event.reply_token, message)
+    # elif '圖片畫廊' in msg:
+    #     message = test()
+    #     line_bot_api.reply_message(event.reply_token, message)
+    # elif '功能' in msg:
+    #     a='1:輸入 『rate』 得知美國公債報價、銀行拆借利率、FED利率、Tips\n2:輸入 『worldequity』\n得知全球股票市場和指數期貨市場報價\n3:輸入 『twstock+股票代碼』\n得知該台股2019年走勢\n4:輸入 『news』\n得知台股與國際股市新聞\n5:輸入 『sectors』\n獲取美股各產業漲跌幅\n6:輸入 『commodity』 取得原物料最新報價\n7:輸入 『BIresearch』 獲取Bloomberg Intelligence研究報告\n8:輸入 『equityprimer』   獲取Bloomberg Intelligence個股研究報告\n9:輸入 『newswebsite』\n進入財經新聞網站\n10:輸入 『tvshows』\n觀看彭博社精選節目'
+    #     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=a))
     else:   # default
         message = TextSendMessage(text=msg)
-        line_bot_api.reply_message(event.reply_token, message)
 
-# def doSQL(int order, str sqlStatement, list data):
-#     try:
-#         # 連接 MySQL/MariaDB 資料庫
-#         connection = mysql.connector.connect(
-#             host='140.119.19.73',          # 主機名稱
-#             port='9306',
-#             database='TG06', # 資料庫名稱
-#             user='TG06',        # 帳號
-#             password='i8p3q6')  # 密碼
+    line_bot_api.reply_message(event.reply_token, message)
 
-#         # 查詢資料庫
-#         cursor = connection.cursor()
-#         if(order==0):
-#             cursor.execute(sqlStatement)
-#         # 列出查詢的資料'
-#             records = cursor.fetchall()
-#             return records
-#             # for (Course_id, Course_name) in cursor:
-#             #     print("Course_id: %s, Course_name: %s" % (Course_id, Course_name))
-#         else:
-#             cursor.execute(sqlStatement,data)
-#             connection.commit()
+def doSQL(order: int, sqlStatement: str, data: list):
+    try:
+        # 連接 MySQL/MariaDB 資料庫
+        connection = mysql.connector.connect(
+            host='140.119.19.73',          # 主機名稱
+            port='9306',
+            database='TG06', # 資料庫名稱
+            user='TG06',        # 帳號
+            password='i8p3q6')  # 密碼
+        
+        if connection.is_connected():
+            print("database version:", connection.get_server_info())
 
-#     except Error as e:
-#         print("資料庫連接失敗：", e)
+        # 查詢資料庫
+        cursor = connection.cursor()
+        cursor.execute("SELECT DATABASE();")
+        record = cursor.fetchone()
+        print("current database:", record)
+        # if(order==0):
+        #     cursor.execute(sqlStatement)
+        # 列出查詢的資料'
+            # records = cursor.fetchall()
+            # return records
+            # for (Course_id, Course_name) in cursor:
+            #     print("Course_id: %s, Course_name: %s" % (Course_id, Course_name))
+        # else:
+        #     cursor.execute(sqlStatement,data)
+        #     connection.commit()
 
-#     finally:
-#         if (connection.is_connected()):
-#             cursor.close()
-#             connection.close()
-#             print("資料庫連線已關閉")
+    except Error as e:
+        print("資料庫連接失敗：", e)
+
+    finally:
+        if (connection.is_connected()):
+            cursor.close()
+            connection.close()
+            print("資料庫連線已關閉")
 
 
 import os
 if __name__ == "__main__":
+    # doSQL(0, "", [])
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
