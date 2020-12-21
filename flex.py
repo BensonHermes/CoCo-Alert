6c21d0837e29fdc8119074f11d17e857ed9b358c
Chou Yun <chowduen@gmail.com>
2020-12-22 02:59:49 +0800

    add flex.py

--- flex.py
+++ flex.py
@@ -6,27 +6,27 @@ def getWarnMapFlex():
          "layout": "vertical",
          "contents": [
             {
-            "type": "box",
-            "layout": "vertical",
-            "margin": "lg",
-            "spacing": "sm",
-            "contents": [
-               {
-                  "type": "box",
-                  "layout": "baseline",
-                  "spacing": "sm",
-                  "contents": [
+               "type": "box",
+               "layout": "vertical",
+               "margin": "lg",
+               "spacing": "sm",
+               "contents": [
                   {
-                     "type": "text",
-                     "text": "在回家的路上可能會經過的危險地點如下：",
-                     "wrap": True,
-                     "color": "#666666",
-                     "size": "sm",
-                     "flex": 5
+                     "type": "box",
+                     "layout": "baseline",
+                     "spacing": "sm",
+                     "contents": [
+                        {
+                           "type": "text",
+                           "text": "在回家的路上可能會經過的危險地點如下：",
+                           "wrap": True,
+                           "color": "#666666",
+                           "size": "sm",
+                           "flex": 5
+                        }
+                     ]
                   }
-                  ]
-               }
-            ]
+               ]
             }
          ]
       },
@@ -36,19 +36,19 @@ def getWarnMapFlex():
          "spacing": "sm",
          "contents": [
             {
-            "type": "button",
-            "style": "link",
-            "height": "sm",
-            "action": {
-               "type": "uri",
-               "label": "打開地圖",
-               "uri": "https://TeresaChou.github.io/WarnMap/"
+               "type": "button",
+               "style": "link",
+               "height": "sm",
+               "action": {
+                  "type": "uri",
+                  "label": "打開地圖",
+                  "uri": "https://TeresaChou.github.io/WarnMap/"
+               }
             }
-            },
-            {
-            "type": "spacer",
-            "size": "sm"
-            }
+            # {
+            #    "type": "spacer",
+            #    "size": "sm"
+            # }
          ],
          "flex": 0
       }
