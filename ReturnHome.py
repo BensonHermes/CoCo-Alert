e931a54259fbb6520c424d22e00c15c89a5716cf
TeresaChou <chowduen@gmail.com>
2021-01-11 17:07:40 +0800

    add demo map

--- ReturnHome.py
+++ ReturnHome.py
@@ -139,9 +139,10 @@ def Demo(line_bot_api, event, BISM, RHSM):
     else:
         contact_info = getContactInfo(user_id)[0]
 
-    note = f"{BISM.info.name}在危險地方已經超過五分鐘了，請快確認他的人身安全吧！\n\
-位置：(24.987556, 121.569168)"
-    message = TextSendMessage(text=note)
+#     note = f"{BISM.info.name}在危險地方已經超過五分鐘了，請快確認他的人身安全吧！\n\
+# 位置：(24.987556, 121.569168)"
+#     message = TextSendMessage(text=note)
+    message = getDemoWarnFlex(contact_info[0])
     line_bot_api.push_message(contact_info[1], message)
 
     RHSM.reset()
